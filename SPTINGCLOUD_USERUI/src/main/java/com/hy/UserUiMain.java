9db6a48122db7f6a76fa3bfde2f3c408bd20c783
flyhuangyang <2419813131@qq.com>
2020-04-13 17:17:21 +0800

    sss

--- SPTINGCLOUD_USERUI/src/main/java/com/hy/UserUiMain.java
+++ SPTINGCLOUD_USERUI/src/main/java/com/hy/UserUiMain.java
@@ -12,9 +12,15 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @RestController
 public class UserUiMain {
+
+    /**
+     * 提供多种访问远程http服务的方法
+     * 是一种访问restful服务的模板类，是spring提供的用户访问rest服务的客户端模板工具集
+     * @return
+     */
     @LoadBalanced
     @Bean
-    public RestTemplate restTemplate() {
+    public RestTemplate restTemplate() {  //
 
         return new RestTemplate();
     }
