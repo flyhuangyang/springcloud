9fc069a8606bb09f27f0fed251904c0778c17fab
flyhuangyang <2419813131@qq.com>
2020-04-13 17:36:45 +0800

    sss

--- SPTINGCLOUD_USERUI/src/main/java/com/hy/userui/controller/UserController.java
+++ SPTINGCLOUD_USERUI/src/main/java/com/hy/userui/controller/UserController.java
@@ -1,31 +1,27 @@
 package com.hy.userui.controller;
 
-import entity.Comm;
 import entity.Result;
 import entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UserController {
+    private static final String REST_URL_PREFIX = "http://localhost:8081";
     @Autowired
     RestTemplate restTemplate;
-    @PostMapping(value="/logins")
-    public String logins(User user){
-        //需要调用userservice这个微服务
-        String url ="http://USERSERVICE/validateUser";
-        ResponseEntity<Result> commResponseEntity = restTemplate.postForEntity(url,user,Result.class);
-        Result body = commResponseEntity.getBody();
-        if(body.getCode()==1){
-            return "redirect:/suc.html";
-        }else{
-            return "redirect:/login.html";
-        }
+    @RequestMapping(value="/user/get/{id}")
+    public User getUser(@PathVariable("id")Long id){
+        return restTemplate.getForObject(REST_URL_PREFIX + "/user/get/" + id, User.class);
+    }
 
-
+    @RequestMapping("/user/add")
+    public Integer addUser(User user) {
+        return restTemplate.postForObject(REST_URL_PREFIX + "/addUser", user, Integer.class);
     }
 }
