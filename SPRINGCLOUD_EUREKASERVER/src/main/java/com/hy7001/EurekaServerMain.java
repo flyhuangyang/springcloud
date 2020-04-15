22d84a3b2667cd41b4be6bb8a42252df35e47ee1
flyhuangyang <2419813131@qq.com>
2020-04-15 09:19:53 +0800

    sss

--- SPRINGCLOUD_EUREKASERVER/src/main/java/com/hy7001/EurekaServerMain.java
+++ SPRINGCLOUD_EUREKASERVER/src/main/java/com/hy7001/EurekaServerMain.java
@@ -1,11 +1,11 @@
-package com.hy;
+package com.hy7001;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer    //启用EurekaServer注册模块
+@EnableEurekaServer    //EurekaServer服务端启动类，接受其它微服务注册进来
 public class EurekaServerMain {
 
     public static void main(String[] args) {
